0e7d0f6a18025d98c328fd85c8282267fb477fee
jjbes <julienaubeb@gmail.com>
2023-04-17 17:21:39 +0900

    fix: Chatbot prompt

--- server/templates/chatbot.py
+++ server/templates/chatbot.py
@@ -1,13 +1,12 @@
 _CHATBOT_TEMPLATE_NO_CONTEXT = \
-"""Instruction: Answer the question.
-Instruction: Reply in the question language.
+"""Instruction: Answer the question in the question language.
 
 Question: "{question}"
 
 A:"""
 
 _CHATBOT_TEMPLATE = \
-"""Instruction: Answer the question in the question language using the following context."
+"""Instruction: Answer the question using the following context."
 Instruction: Reply in the question language.
 
 Context: "{context}"
