345bc4380d34e72e7b9a52bcc52949441418bf26
jjbes <julienaubeb@gmail.com>
2023-05-01 14:45:18 +0900

    feat: disable highlight

--- src/pages/components/prototype/epub-reader/components/reader.tsx
+++ src/pages/components/prototype/epub-reader/components/reader.tsx
@@ -15,6 +15,7 @@ export default ({ file, currentLocation, setPageContent, setExcerpt}: ReaderProp
     const [url, _] = useState(URL.createObjectURL(file))
     const [location, setLocation] = useState<string>("0")
     const [nbReqLoading, setNbReqLoading] = useState<number>(0)
+    const [displayHighlight, setDisplayHighlight] = useState<boolean>(false)
     const renditionRef = useRef<any>(null)
 
     const locationChanged = (epubcifi: string) => {
@@ -100,6 +101,8 @@ export default ({ file, currentLocation, setPageContent, setExcerpt}: ReaderProp
 
     //Set Highlights
     useEffect(() => {
+        if(!displayHighlight) return
+
         if(!location) return
         if(!location.includes("epubcfi")) return
 
@@ -110,7 +113,6 @@ export default ({ file, currentLocation, setPageContent, setExcerpt}: ReaderProp
         if (excerptList.includes(section.canonical)) return
         excerptList.push(section.canonical)
         
-          
         const chunks = stringToChunks(section.contents.textContent, 1000)
 
         chunks.forEach((context)=>{
@@ -172,7 +174,22 @@ export default ({ file, currentLocation, setPageContent, setExcerpt}: ReaderProp
             })
         })
         
-    }, [location])
+    }, [location, displayHighlight])
+
+    useEffect(() => {
+        const highlights = document.getElementsByClassName("highlight")
+        console.log(highlights)
+        if(!displayHighlight) {
+            for (let i = 0; i < highlights.length; i++) {
+                highlights.item(i).style.display = "none"
+            }
+        } else {
+            for (let i = 0; i < highlights.length; i++) {
+                highlights.item(i).style.display = "block"
+            }
+        }
+        
+    }, [displayHighlight])
 
     return (
         <div className='h-full w-full flex flex-row relative'>
@@ -189,16 +206,22 @@ export default ({ file, currentLocation, setPageContent, setExcerpt}: ReaderProp
                     }}
                 />
             </div>
+            <button className={(nbReqLoading>0 ? "cursor-not-allowed " : "") + (displayHighlight ? "bg-slate-200 ring ring-slate-300 ": "bg-slate-100 ring ring-slate-100 ") + "h-8 w-8 absolute top-[10px] right-[10px] z-50 flex items-center justify-center rounded"}
+                onClick={() => {
+                    if(nbReqLoading<=0){
+                        setDisplayHighlight(!displayHighlight)
+                    }
+                }}>
             {
                 nbReqLoading>0 ?
-                <div className='absolute top-[10px] right-0 z-50'>
-                    <svg className="animate-spin -ml-1 mr-3 h-5 w-5 text-gray" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
-                        <circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" strokeWidth="4"></circle>
-                        <path className="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
-                    </svg>
-                </div> : 
-                <></>
+                <svg className="animate-spin h-full w-full text-gray p-2" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+                    <circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" strokeWidth="4"></circle>
+                    <path className="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
+                </svg>
+                : 
+                <>🖊️</>
             }
+            </button>
         </div>
        
     )
