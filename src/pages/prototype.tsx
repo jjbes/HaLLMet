2398493d4cdfc738c01daffa80fb7771d3518f26
jjbes <julienaubeb@gmail.com>
2023-05-03 15:38:23 +0900

    refactor: removed useless check

--- src/pages/prototype.tsx
+++ src/pages/prototype.tsx
@@ -8,21 +8,11 @@ export default () => {
     const [highlightExcerpt, setHighlightExcerpt] = useState<string|null>(null)
     const [pageContent, setPageContent] = useState<string|null>()
     const [sectionContent, setSectionContent] = useState<string|null>()
-    const [context, setContext] = useState<string|null>()
     const currentLocation = useRef<any>(null)
-
-    useEffect(() => {
-        if(true) {
-            setContext(sectionContent)
-        }else
-        if(false) {
-            setContext(pageContent)
-        }
-    }, [pageContent, sectionContent])
 
     return (
         <>
-        <Emotion currentPage={currentLocation.current ? currentLocation.current.page : null} context={pageContent?pageContent:null}/>
+        <Emotion currentPage={currentLocation.current ? currentLocation.current.page : null} context={pageContent?pageContent:''}/>
         <div className="h-full flex justify-center ">
             <div className="w-2/4 px-4 pt-4 text-center text-gray-700">
                 <EpubReader 
@@ -32,7 +22,7 @@ export default () => {
                     currentLocation={currentLocation}
                 />
             </div>
-            <Explanation context={context?context:''} excerpt={highlightExcerpt} setExcerpt={setHighlightExcerpt} currentSection={currentLocation.current ? currentLocation.current.section : null}/>       
+            <Explanation context={sectionContent?sectionContent:''} excerpt={highlightExcerpt} setExcerpt={setHighlightExcerpt} currentSection={currentLocation.current ? currentLocation.current.section : null}/>       
         </div>
         </>
         
